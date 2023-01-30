6f3e74ce2c7d63db90dcdb92b181f0e3214b9b00
KrishPatel08 <KrishUmeshbhai.Patel@flemingcollege.ca>
2023-01-30 12:47:44 -0500

    create script file
    create a complex data structure

---
+++ script.py
@@ -0,0 +1,30 @@
+def main():
+    about_me = {
+        'full_name' : 'Krish Umeshbhai Patel',
+        'student_id' : '10291361',
+        'pizza_toppings' : [
+            'ONIONS',
+            'BABY CORNS',
+            'PANNER'
+        ],
+        'movies' : [
+            {
+                'title' : 'avengers',
+                'genre' : 'action',
+            },
+            {
+                'title' : 'jungle cruse',
+                'genre' : 'advanture'
+            }
+
+        ]
+    }
+
+    
+    
+
+
+
+
+if __name__== '__main__':
+    main()
